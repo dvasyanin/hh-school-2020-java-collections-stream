063def0f3c82d19075912bf6bb50a5db3c020c67
Dmitry Vasyanin <dvasyanin0@gmail.com>
2020-11-30 02:59:55 +0300

    solved Task8

--- src/tasks/Task8.java
+++ src/tasks/Task8.java
@@ -3,12 +3,7 @@ package tasks;
 import common.Person;
 import common.Task;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,75 +18,57 @@ P.P.S Здесь ваши правки желательно прокоммент
  */
 public class Task8 implements Task {
 
-  private long count;
+    private long count;
 
-  //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
-  }
-
-  //ну и различные имена тоже хочется
-  public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
-  }
-
-  //Для фронтов выдадим полное имя, а то сами не могут
-  public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
+    //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
+    public List<String> getNames(List<Person> persons) {
+        // скипаем первый элемент, с остальными работаем
+        return persons.stream()
+                .skip(1)
+                .map(Person::getFirstName)
+                .collect(Collectors.toList());
     }
 
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
+    //ну и различные имена тоже хочется
+    public Set<String> getDifferentNames(List<Person> persons) {
+        // хеш сет гарантирует уникальность
+        return new HashSet<>(getNames(persons));
     }
 
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
-    }
-    return result;
-  }
-
-  // словарь id персоны -> ее имя
-  public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
-    }
-    return map;
-  }
-
-  // есть ли совпадающие в двух коллекциях персоны?
-  public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
+    //Для фронтов выдадим полное имя, а то сами не могут
+    public String convertPersonToString(Person person) {
+        // склеиваем за раз все в одну строку и отдаем
+        if ((person.getSecondName() != null) && (person.getFirstName() != null)) {
+            return person.getSecondName() + " " + person.getFirstName();
         }
-      }
+        return "";
     }
-    return has;
-  }
 
-  //...
-  public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
-  }
+    // словарь id персоны -> ее имя
+    public Map<Integer, String> getPersonNames(Collection<Person> persons) {
+        return persons.stream()
+                .collect(Collectors.toMap(Person::getId, this::convertPersonToString));
+    }
 
-  @Override
-  public boolean check() {
-    System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
-    boolean codeSmellsGood = false;
-    boolean reviewerDrunk = false;
-    return codeSmellsGood || reviewerDrunk;
-  }
+    // есть ли совпадающие в двух коллекциях персоны?
+    public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
+        // anyMatch() вернет true в том случае, если в Stream-e есть хоть один элемент, что удовлетворяет условию
+        // можно преобразовать вторую коллекцию в сет, для быстродействия
+        return persons1.stream()
+                .anyMatch(persons2::contains);
+    }
+
+    //...
+    public long countEven(Stream<Integer> numbers) {
+        // отфильтровали, посчитали каунт
+        return numbers.filter(num -> num % 2 == 0).count();
+    }
+
+    @Override
+    public boolean check() {
+        System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
+        boolean codeSmellsGood = true;
+        boolean reviewerDrunk = false;
+        return codeSmellsGood || reviewerDrunk;
+    }
 }
